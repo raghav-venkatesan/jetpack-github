b654410199b7df531f1df6286c0d328142bf2a2c
Raghav Venkatesan <raghav.hv@gmail.com>
2018-08-12 01:43:40 -0400

    Implement card view and transparent background in Bottom Sheet Dialog

--- app/src/main/java/com/example/raghav/jetpackgithub/ui/ReposListFragment.kt
+++ app/src/main/java/com/example/raghav/jetpackgithub/ui/ReposListFragment.kt
@@ -1,12 +1,12 @@
 package com.example.raghav.jetpackgithub.ui
 
-
 import android.os.Bundle
 import android.support.design.widget.BottomSheetDialog
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import com.example.raghav.jetpackgithub.R
 import kotlinx.android.synthetic.main.fragment_repos_list.*
 
@@ -29,6 +29,8 @@ class ReposListFragment : Fragment() {
         searchButton.setOnClickListener {
             val bottomSheetDialog = BottomSheetDialog(context!!)
             bottomSheetDialog.setContentView(R.layout.view_repo_detail)
+            // Find the frame layout within the coordinate layout of the bottom sheet dialog to make it transparent
+            bottomSheetDialog.window?.findViewById<FrameLayout>(R.id.design_bottom_sheet)?.setBackgroundResource(android.R.color.transparent);
             bottomSheetDialog.show()
         }
     }
