dfe1100700b19e7138bf5e3a71bde67c6bff58fd
Raghav Venkatesan <raghav.hv@gmail.com>
2018-08-12 00:53:21 -0400

    Remove fragment implementation and use BottomSheetDialog for displaying repo detail

--- app/src/main/java/com/example/raghav/jetpackgithub/ui/ReposListFragment.kt
+++ app/src/main/java/com/example/raghav/jetpackgithub/ui/ReposListFragment.kt
@@ -2,12 +2,13 @@ package com.example.raghav.jetpackgithub.ui
 
 
 import android.os.Bundle
+import android.support.design.widget.BottomSheetDialog
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.example.raghav.jetpackgithub.R
+import kotlinx.android.synthetic.main.fragment_repos_list.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,8 +20,17 @@ class ReposListFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_repos_list, container, false)
-
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        searchButton.setOnClickListener {
+            val bottomSheetDialog = BottomSheetDialog(context!!)
+            bottomSheetDialog.setContentView(R.layout.view_repo_detail)
+            bottomSheetDialog.show()
+        }
     }
 
 
