6654f58dac095c058e7a01a0a8b7c79a940773a7
Raghav Venkatesan <raghav.hv@gmail.com>
2018-08-13 05:26:56 -0400

    Properly format date and time

--- app/src/main/java/com/example/raghav/jetpackgithub/util/DateUtil.kt
+++ app/src/main/java/com/example/raghav/jetpackgithub/util/DateUtil.kt
@@ -6,5 +6,5 @@ import java.util.*
 fun convertTimeFormat(time: String): String {
     val inputDf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
     val outputDf = SimpleDateFormat("MMM d, yyyy h:mm:ss a", Locale.getDefault())
-    return outputDf.format(inputDf.parse(time))
+    return outputDf.format(inputDf.parse(time)).replace("a.m.", "AM").replace("p.m.", "PM")
 }
